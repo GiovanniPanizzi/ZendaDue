69769cee7e21503e9a5326ab91c5761937974711
GIOVANNI_PANIZZI <giovannipanizzi5@gmail.com>
2025-03-01 19:36:11 +0100

    animazione coso in alto mobile

--- hamburgermenu.js
+++ hamburgermenu.js
@@ -1,18 +1,29 @@
                 const hamburger = document.querySelector(".hamburger-menu");
                 const mobileNav = document.querySelector(".mobile-menu");
                 const mobileNavItemLink = document.querySelectorAll(".mobile-nav-item-link");
+                let isActive = mobileNav.classList.contains ("active");
+                let isNotActive = mobileNav.classList.contains ("inactive");
 
                 hamburger.addEventListener ("click", () => {
-                    hamburger.classList.toggle ("active");
-                    hamburger.classList.toggle ("inactive");
-                    mobileNav.classList.toggle ("active");
+
+                    if(isActive && !isNotActive){
+                        mobileNav.classList.add("inactive");
+                        mobileNav.classList.remove("active");
+                        setTimeout(() => mobileNav.classList.remove("inactive"), 1000);
+                    }
+                    else{
+                        mobileNav.classList.add("active");
+                        mobileNav.classList.remove("inactive");
+
+                    }
+                    isActive = !isActive;
                 });
                 
                 mobileNavItemLink.forEach(item => {
                     item.addEventListener("click", () => {
-                    console.log("ciao");
                     hamburger.classList.remove("active");
                     hamburger.classList.add("inactive");
                     mobileNav.classList.remove("active");
+                    mobileNav.classList.toggle ("inactive");
                     });
                 });
